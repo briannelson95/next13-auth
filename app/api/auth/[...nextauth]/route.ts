1e81afd8dbcc6c8af44f2c3a1571d08ac9d81bdd
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-04-24 19:11:35 -0400

    added prisma

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github"
 
+const prisma = new PrismaClient();
+
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
